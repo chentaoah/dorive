157d0d5e573ff48ed6d484fa8c315a64c87908aa
tao.chen1 <tao.chen1@yintech.cn>
2022-03-23 17:12:08 +0800

    优化部分反射逻辑

--- spring-domain-proxy/src/main/java/com/gitee/spring/domain/proxy/utils/ReflectUtils.java
+++ spring-domain-proxy/src/main/java/com/gitee/spring/domain/proxy/utils/ReflectUtils.java
@@ -1,20 +1,12 @@
 package com.gitee.spring.domain.proxy.utils;
 
-import cn.hutool.core.lang.Assert;
-
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Type;
-import java.util.Objects;
 
 public class ReflectUtils {
 
     public static Type getGenericSuperclass(Object object, Class<?> targetClass) {
-        Class<?> clazz = object.getClass();
-        while (clazz != null && clazz != Object.class && clazz != targetClass) {
-            clazz = clazz.getSuperclass();
-        }
-        Assert.isTrue(clazz != null && clazz != Object.class, "Failed to get superclass of target type!");
-        return Objects.requireNonNull(clazz).getGenericSuperclass();
+        return object.getClass().getGenericSuperclass();
     }
 
     public static Constructor<?> getConstructor(Class<?> type, Class<?>[] parameterTypes) {
