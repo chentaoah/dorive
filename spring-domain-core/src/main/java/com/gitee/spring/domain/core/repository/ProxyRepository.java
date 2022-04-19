4a860fc6a910c686c5768e311737a41bbb76ecb4
chentaoah <609580885@qq.com>
2022-04-19 19:43:51 +0800

    细化仓储实现类

---
+++ spring-domain-core/src/main/java/com/gitee/spring/domain/core/repository/ProxyRepository.java
@@ -0,0 +1,62 @@
+package com.gitee.spring.domain.core.repository;
+
+import com.gitee.spring.domain.core.entity.BoundedContext;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+public class ProxyRepository extends AbstractRepository<Object, Object> {
+
+    protected AbstractRepository<Object, Object> repository;
+
+    @Override
+    public Object selectByPrimaryKey(BoundedContext boundedContext, Object primaryKey) {
+        return repository.selectByPrimaryKey(boundedContext, primaryKey);
+    }
+
+    @Override
+    public List<Object> selectByExample(BoundedContext boundedContext, Object example) {
+        return repository.selectByExample(boundedContext, example);
+    }
+
+    @Override
+    public <T> T selectPageByExample(BoundedContext boundedContext, Object example, Object page) {
+        return repository.selectPageByExample(boundedContext, example, page);
+    }
+
+    @Override
+    public int insert(BoundedContext boundedContext, Object entity) {
+        return repository.insert(boundedContext, entity);
+    }
+
+    @Override
+    public int update(BoundedContext boundedContext, Object entity) {
+        return repository.update(boundedContext, entity);
+    }
+
+    @Override
+    public int updateByExample(Object entity, Object example) {
+        return repository.updateByExample(entity, example);
+    }
+
+    @Override
+    public int delete(BoundedContext boundedContext, Object entity) {
+        return repository.delete(boundedContext, entity);
+    }
+
+    @Override
+    public int deleteByPrimaryKey(Object primaryKey) {
+        return repository.deleteByPrimaryKey(primaryKey);
+    }
+
+    @Override
+    public int deleteByExample(Object example) {
+        return repository.deleteByExample(example);
+    }
+
+}
