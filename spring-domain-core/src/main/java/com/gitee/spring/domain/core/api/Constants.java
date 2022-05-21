ef95b39d80e83f8f0333f2dfe208f54ce79d21d1
chentaoah <609580885@qq.com>
2022-05-22 00:07:24 +0800

    调整代码格式

--- spring-domain-core/src/main/java/com/gitee/spring/domain/core/api/Constants.java
+++ spring-domain-core/src/main/java/com/gitee/spring/domain/core/api/Constants.java
@@ -4,10 +4,10 @@ public interface Constants {
     String NAME_ATTRIBUTE = "name";
     String SCENE_ATTRIBUTE = "scene";
     String MAPPER_ATTRIBUTE = "mapper";
-    String ASSEMBLER_ATTRIBUTE = "assembler";
-    String REPOSITORY_ATTRIBUTE = "repository";
     String MAP_AS_EXAMPLE_ATTRIBUTE = "mapAsExample";
     String ORDER_ATTRIBUTE = "order";
+    String ASSEMBLER_ATTRIBUTE = "assembler";
+    String REPOSITORY_ATTRIBUTE = "repository";
     String FIELD_ATTRIBUTE = "field";
     String ALIAS_ATTRIBUTE = "alias";
     String BIND_ATTRIBUTE = "bind";
