20d59a83b2f5205e099a61ff9c08b4020fb9075a
tao.chen1 <tao.chen1@yintech.cn>
2022-07-13 09:56:06 +0800

    修复一个打包报错问题，并升级版本到2.6.3

--- spring-domain-core/src/main/java/com/gitee/spring/domain/core/entity/BoundedContext.java
+++ spring-domain-core/src/main/java/com/gitee/spring/domain/core/entity/BoundedContext.java
@@ -1,15 +1,11 @@
 package com.gitee.spring.domain.core.entity;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
 
 import java.util.LinkedHashMap;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 @EqualsAndHashCode(callSuper = false)
 public class BoundedContext extends LinkedHashMap<String, Object> {
 }
