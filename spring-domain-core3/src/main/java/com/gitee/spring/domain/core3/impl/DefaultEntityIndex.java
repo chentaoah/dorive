c3642401a71ad72fdcd303407e893053a742c0d7
tao.chen1 <tao.chen1@yintech.cn>
2022-10-28 16:49:33 +0800

    优化索引的主键类型

--- spring-domain-core3/src/main/java/com/gitee/spring/domain/core3/impl/DefaultEntityIndex.java
+++ spring-domain-core3/src/main/java/com/gitee/spring/domain/core3/impl/DefaultEntityIndex.java
@@ -1,6 +1,7 @@
 package com.gitee.spring.domain.core3.impl;
 
 import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.convert.Convert;
 import com.gitee.spring.domain.core3.api.EntityIndex;
 
 import java.util.ArrayList;
@@ -11,29 +12,31 @@ import java.util.Map;
 
 public class DefaultEntityIndex implements EntityIndex {
 
-    private final Map<Object, List<Object>> primaryKeyMapping = new HashMap<>();
-    private final Map<Object, Object> primaryKeyEntityMap = new HashMap<>();
+    private final Map<Long, List<Long>> primaryKeyMapping = new HashMap<>();
+    private final Map<Long, Object> primaryKeyEntityMap = new HashMap<>();
 
     public DefaultEntityIndex(List<Map<String, Object>> resultMaps, List<Object> entities) {
         for (Map<String, Object> resultMap : resultMaps) {
-            Object rootPrimaryKey = resultMap.get("$id");
-            Object primaryKey = resultMap.get("id");
-            List<Object> primaryKeys = primaryKeyMapping.computeIfAbsent(rootPrimaryKey, key -> new ArrayList<>());
+            Long rootPrimaryKey = (Long) resultMap.get("$id");
+            Long primaryKey = (Long) resultMap.get("id");
+            List<Long> primaryKeys = primaryKeyMapping.computeIfAbsent(rootPrimaryKey, key -> new ArrayList<>());
             primaryKeys.add(primaryKey);
         }
         for (Object entity : entities) {
             Object primaryKey = BeanUtil.getFieldValue(entity, "id");
-            primaryKeyEntityMap.put(primaryKey, entity);
+            Long number = Convert.convert(Long.class, primaryKey);
+            primaryKeyEntityMap.put(number, entity);
         }
     }
 
     @Override
     public List<Object> selectList(Object rootEntity) {
         Object rootPrimaryKey = BeanUtil.getFieldValue(rootEntity, "id");
-        List<Object> primaryKeys = primaryKeyMapping.get(rootPrimaryKey);
+        Long number = Convert.convert(Long.class, rootPrimaryKey);
+        List<Long> primaryKeys = primaryKeyMapping.get(number);
         if (primaryKeys != null && !primaryKeys.isEmpty()) {
             List<Object> entities = new ArrayList<>(primaryKeys.size());
-            for (Object primaryKey : primaryKeys) {
+            for (Long primaryKey : primaryKeys) {
                 Object entity = primaryKeyEntityMap.get(primaryKey);
                 entities.add(entity);
             }
