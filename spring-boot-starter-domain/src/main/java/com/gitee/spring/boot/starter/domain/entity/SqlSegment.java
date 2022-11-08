c4ed8a765edd2e50068b5923bc0d37710c6194c1
tao.chen1 <tao.chen1@yintech.cn>
2022-11-08 12:49:05 +0800

    【优化】实现智能拼接sql

--- spring-boot-starter-domain/src/main/java/com/gitee/spring/boot/starter/domain/entity/SqlSegment.java
+++ spring-boot-starter-domain/src/main/java/com/gitee/spring/boot/starter/domain/entity/SqlSegment.java
@@ -8,9 +8,16 @@ import java.util.Set;
 @Data
 @AllArgsConstructor
 public class SqlSegment {
+    
     private String sql;
     private String tableName;
     private String tableAlias;
     private boolean toHandle;
     private Set<String> dependentTables;
+
+    @Override
+    public String toString() {
+        return sql;
+    }
+
 }
