2a8e66e5deb2e0c53baa0cbb250efc7f7cf3ffeb
tao.chen1 <tao.chen1@yintech.cn>
2022-11-28 16:28:22 +0800

    优化判断逻辑

--- dorive-core/src/main/java/com/gitee/dorive/core/impl/handler/BatchEntityHandler.java
+++ dorive-core/src/main/java/com/gitee/dorive/core/impl/handler/BatchEntityHandler.java
@@ -42,17 +42,18 @@ public class BatchEntityHandler implements EntityHandler {
     @Override
     public void handleEntities(BoundedContext boundedContext, List<Object> rootEntities) {
         for (ConfiguredRepository repository : this.repository.getSubRepositories()) {
-            if (repository.matchKeys(boundedContext)) {
-                UnionExample unionExample = newUnionExample(repository, boundedContext, rootEntities);
-                if (!unionExample.isDirtyQuery()) {
-                    continue;
-                }
-                Result<Object> result = repository.selectResultByExample(boundedContext, unionExample);
-                if (!(result instanceof EntityIndex)) {
-                    continue;
-                }
-                setValueForRootEntities(repository, rootEntities, (EntityIndex) result);
+            if (!repository.matchKeys(boundedContext)) {
+                continue;
             }
+            UnionExample unionExample = newUnionExample(repository, boundedContext, rootEntities);
+            if (!unionExample.isDirtyQuery()) {
+                continue;
+            }
+            Result<Object> result = repository.selectResultByExample(boundedContext, unionExample);
+            if (!(result instanceof EntityIndex)) {
+                continue;
+            }
+            setValueForRootEntities(repository, rootEntities, (EntityIndex) result);
         }
     }
 
