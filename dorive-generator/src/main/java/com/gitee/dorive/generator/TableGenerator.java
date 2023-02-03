9a775f84aeca1381452dd739c8ec6ef3aa1f482f
tao.chen1 <tao.chen1@yintech.cn>
2023-02-03 13:40:37 +0800

    简化方法

--- dorive-generator/src/main/java/com/gitee/dorive/generator/TableGenerator.java
+++ dorive-generator/src/main/java/com/gitee/dorive/generator/TableGenerator.java
@@ -1,14 +1,17 @@
 package com.gitee.dorive.generator;
 
 import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.util.StrUtil;
 import com.gitee.dorive.generator.entity.ClassVO;
 import com.gitee.dorive.generator.entity.TableInfo;
 import com.gitee.dorive.generator.impl.Doclet;
 import com.gitee.dorive.generator.util.TableUtils;
 
 import java.io.File;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class TableGenerator {
 
@@ -25,6 +28,25 @@ public class TableGenerator {
             return TableUtils.getTableInfos(tablePrefix, classVOs);
         }
         return null;
+    }
+
+    public static void sysout(String tablePrefix, String dirPath) {
+        List<TableInfo> tableInfos = execute(tablePrefix, dirPath);
+        if (tableInfos != null) {
+            List<String> tableNames = tableInfos.stream().map(TableInfo::getTableName).collect(Collectors.toList());
+            List<String> tableSqls = tableInfos.stream().map(TableInfo::getTableSql).collect(Collectors.toList());
+            System.out.println(StrUtil.join(", ", tableNames) + "\n");
+            System.out.println(StrUtil.join("\n\n", tableSqls));
+        }
+    }
+
+    public static void outputFile(String tablePrefix, String dirPath, String outputPath) {
+        List<TableInfo> tableInfos = execute(tablePrefix, dirPath);
+        if (tableInfos != null) {
+            List<String> tableSqls = tableInfos.stream().map(TableInfo::getTableSql).collect(Collectors.toList());
+            String content = StrUtil.join("\n\n", tableSqls);
+            FileUtil.writeString(content, outputPath, StandardCharsets.UTF_8);
+        }
     }
 
 }
