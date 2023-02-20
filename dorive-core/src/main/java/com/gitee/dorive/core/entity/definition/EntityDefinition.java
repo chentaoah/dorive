0c2165263e76250ed1fe579566aa731ee18da805
chentaoah <609580885@qq.com>
2023-02-20 22:29:02 +0800

    支持自定义复杂查询器

--- dorive-core/src/main/java/com/gitee/dorive/core/entity/definition/EntityDefinition.java
+++ dorive-core/src/main/java/com/gitee/dorive/core/entity/definition/EntityDefinition.java
@@ -34,7 +34,6 @@ import java.util.Map;
 public class EntityDefinition {
 
     private String name;
-    @Deprecated
     private String[] scenes;
     private int order;
     private Class<?> mapper;
