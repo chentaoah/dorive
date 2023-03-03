dc1aac83e29bae25b4d876278a96ec76036affb4
tao.chen1 <tao.chen1@yintech.cn>
2023-03-03 14:08:19 +0800

    3.3.1-pre

--- dorive-api/src/main/java/com/gitee/dorive/api/impl/resolver/EntityResolver.java
+++ dorive-api/src/main/java/com/gitee/dorive/api/impl/resolver/EntityResolver.java
@@ -1,15 +1,12 @@
 package com.gitee.dorive.api.impl.resolver;
 
-import com.gitee.dorive.api.entity.def.BindingDef;
 import com.gitee.dorive.api.entity.element.EntityEle;
 import com.gitee.dorive.api.entity.element.EntityField;
 import com.gitee.dorive.api.entity.element.EntityType;
-import com.gitee.dorive.api.util.PathUtils;
 import lombok.Data;
 
 import java.util.Collection;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 
 @Data
@@ -27,22 +24,10 @@ public class EntityResolver {
     private void resolve(String lastAccessPath, Collection<EntityField> entityFields) {
         for (EntityField entityField : entityFields) {
             String accessPath = lastAccessPath + "/" + entityField.getName();
-            renewDef(accessPath, entityField);
             entityEleMap.put(accessPath, entityField);
             EntityType entityType = entityField.getEntityType();
             if (entityType != null) {
                 resolve(accessPath, entityType.getEntityFields().values());
-            }
-        }
-    }
-
-    private void renewDef(String accessPath, EntityField entityField) {
-        List<BindingDef> bindingDefs = entityField.getBindingDefs();
-        for (BindingDef bindingDef : bindingDefs) {
-            String bindExp = bindingDef.getBindExp();
-            if (bindExp.startsWith(".")) {
-                bindExp = PathUtils.getAbsolutePath(accessPath, bindExp);
-                bindingDef.setBindExp(bindExp);
             }
         }
     }
