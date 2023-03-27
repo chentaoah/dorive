6cc22999ebdbdd8123edda203c789f37102154bf
tao.chen1 <tao.chen1@yintech.cn>
2023-03-27 10:07:27 +0800

    调整代码

--- dorive-api/src/main/java/com/gitee/dorive/api/constant/Operator.java
+++ dorive-api/src/main/java/com/gitee/dorive/api/constant/Operator.java
@@ -20,14 +20,14 @@ package com.gitee.dorive.api.constant;
 public interface Operator {
     String EQ = "=";
     String NE = "<>";
-    String IN = "IN";
-    String NOT_IN = "NOT IN";
-    String IS_NULL = "IS NULL";
-    String IS_NOT_NULL = "IS NOT NULL";
-    String LIKE = "LIKE";
-    String NOT_LIKE = "NOT LIKE";
     String GT = ">";
     String GE = ">=";
     String LT = "<";
     String LE = "<=";
+    String IN = "IN";
+    String NOT_IN = "NOT IN";
+    String LIKE = "LIKE";
+    String NOT_LIKE = "NOT LIKE";
+    String IS_NULL = "IS NULL";
+    String IS_NOT_NULL = "IS NOT NULL";
 }
