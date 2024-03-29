cdb0d86ce95692a46f89ec9a8961996bfa0c5df5
tao.chen1 <tao.chen1@yintech.cn>
2024-03-29 18:27:14 +0800

    精简注释，添加wiki地址

--- dorive-api/src/main/java/com/gitee/dorive/api/annotation/Binding.java
+++ dorive-api/src/main/java/com/gitee/dorive/api/annotation/Binding.java
@@ -21,30 +21,7 @@ import java.lang.annotation.*;
 
 /**
  * 绑定注解<br>
- * 作用：声明实体字段之间的绑定关系<br>
- *
- * <p>
- * 绑定类型有三种，分别是：<br>
- * 1、强绑定（两个字段强相关时使用）<br>
- * <pre>{@code
- * @Binding(field = "field", bindExp = "./field")
- * }</pre>
- * 2、弱绑定（字段与上下文相关时使用）<br>
- * <pre>{@code
- * @Binding(field = "field", processExp = "#ctx['field']")
- * }</pre>
- * 3、值绑定（字段与字面值相关时使用）<br>
- * <pre>{@code
- * @Binding(value = "0", bindExp = "./field")
- * }</pre>
- * </p>
- *
- * <p>
- * 当绑定的字段，还需要再进行深度解析时，请使用以下配置方式：<br>
- * <pre>{@code
- * @Binding(field = "field", bindExp = "./list", processExp = "#val.![field]", bindField = "field")
- * }</pre>
- * </p>
+ * wiki：https://gitee.com/digital-engine/dorive/wikis/pages
  *
  * @author tao.chen
  */
@@ -56,45 +33,34 @@ import java.lang.annotation.*;
 public @interface Binding {
 
     /**
-     * 字段名称<br>
-     * 作用：声明当前实体绑定的字段<br>
+     * 字段名称
      */
     String field() default "";
 
     /**
-     * 字面值<br>
-     * 作用：值绑定时使用<br>
+     * 字面值
      */
     String value() default "";
 
     /**
-     * 绑定表达式<br>
-     * 作用：声明上下文绑定的字段<br>
-     * 说明：<br>
-     * 1、./field是以当前实体为参考系的相对路径<br>
-     * 2、/field是以聚合根为参考系的绝对路径<br>
+     * 绑定表达式
      */
     String bindExp() default "";
 
     /**
-     * 加工表达式<br>
-     * 作用：从上下文中获取信息，或加工绑定的字段<br>
-     * 说明：表达式书写请参考SpEL<br>
+     * 加工表达式
      *
      * @see org.springframework.expression
      */
     String processExp() default "";
 
     /**
-     * 指定加工器<br>
-     * 作用：声明字段加工的具体实现<br>
-     * 说明：如果加工表达式不为空，默认实现为SpELProcessor<br>
+     * 指定加工器
      */
     Class<?> processor() default Object.class;
 
     /**
-     * 绑定的字段名称<br>
-     * 作用：显式声明绑定的字段名称<br>
+     * 绑定的字段名称
      */
     String bindField() default "";
 
