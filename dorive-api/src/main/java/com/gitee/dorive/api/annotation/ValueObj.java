74159e73a929cff5c0266f570db71e796ac8ed19
tao.chen1 <tao.chen1@yintech.cn>
2024-04-19 18:05:07 +0800

    修改注释

--- dorive-api/src/main/java/com/gitee/dorive/api/annotation/ValueObj.java
+++ dorive-api/src/main/java/com/gitee/dorive/api/annotation/ValueObj.java
@@ -39,4 +39,7 @@ public @interface ValueObj {
     @AliasFor(annotation = Field.class)
     boolean valueObj() default true;
 
+    @AliasFor(annotation = Field.class)
+    Class<?> converter() default Object.class;
+
 }
