1f2cc99444d72ba4428b5034ca9d4f5535ae13d0
chentaoah <609580885@qq.com>
2024-06-24 22:10:43 +0800

    调整代码

--- dorive-api/src/main/java/com/gitee/dorive/api/ele/FieldElement.java
+++ dorive-api/src/main/java/com/gitee/dorive/api/ele/FieldElement.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.gitee.dorive.api.ele;
 
 import com.gitee.dorive.api.def.FieldDef;
@@ -20,7 +37,8 @@ public class FieldElement {
     }
 
     public boolean isSameType(FieldElement fieldElement) {
-        return fieldDefinition.getTypeName().equals(fieldElement.getFieldDefinition().getTypeName()) && fieldDefinition.getGenericTypeName().equals(fieldElement.getFieldDefinition().getGenericTypeName());
+        return fieldDefinition.getTypeName().equals(fieldElement.getFieldDefinition().getTypeName())
+                && fieldDefinition.getGenericTypeName().equals(fieldElement.getFieldDefinition().getGenericTypeName());
     }
 
 }
