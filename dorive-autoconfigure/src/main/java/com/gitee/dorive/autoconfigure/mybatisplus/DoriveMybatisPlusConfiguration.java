a3fd39606d87f7ab1d975178c2abbdb593c09bf7
chentaoah <609580885@qq.com>
2025-04-01 23:19:56 +0800

    将自动配置独立出来

--- dorive-autoconfigure/src/main/java/com/gitee/dorive/autoconfigure/mybatisplus/DoriveMybatisPlusConfiguration.java
+++ dorive-autoconfigure/src/main/java/com/gitee/dorive/autoconfigure/mybatisplus/DoriveMybatisPlusConfiguration.java
@@ -17,15 +17,57 @@
 
 package com.gitee.dorive.autoconfigure.mybatisplus;
 
-import com.gitee.dorive.mybatis.plus.impl.DefaultImplFactory;
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import com.gitee.dorive.core.api.common.ImplFactory;
+import com.gitee.dorive.mybatis.plus.impl.DefaultImplFactory;
+import com.gitee.dorive.mybatis.plus.injector.EasySqlInjector;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.env.EnvironmentPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.PropertiesPropertySource;
+import org.springframework.core.env.PropertySource;
+
+import java.util.Properties;
 
 @Order(-100)
 @Configuration
-public class DoriveMybatisPlusConfiguration {
+public class DoriveMybatisPlusConfiguration implements EnvironmentPostProcessor {
+
+    @Override
+    public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
+        Properties properties = new Properties();
+        addPropertyIfAbsent(environment, properties, "mybatis-plus.global-config.enable-sql-runner", true);
+        if (!properties.isEmpty()) {
+            PropertySource<?> propertySource = new PropertiesPropertySource(this.getClass().getName() + "@KeyValues", properties);
+            environment.getPropertySources().addLast(propertySource);
+        }
+    }
+
+    private void addPropertyIfAbsent(ConfigurableEnvironment environment, Properties properties, String key, Object value) {
+        if (!environment.containsProperty(key)) {
+            properties.setProperty(key, String.valueOf(value));
+        }
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public EasySqlInjector easySqlInjector() {
+        return new EasySqlInjector();
+    }
 
     @Bean
     public static ImplFactory implFactory() {
