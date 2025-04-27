9434394abf41213bd752aa98ae8a8389f7afa376
chentaoah <609580885@qq.com>
2025-04-27 22:58:01 +0800

    支持测试案例直接引入

--- dorive-module/src/main/java/com/gitee/dorive/module/impl/parser/AbstractModuleParser.java
+++ dorive-module/src/main/java/com/gitee/dorive/module/impl/parser/AbstractModuleParser.java
@@ -87,6 +87,7 @@ public abstract class AbstractModuleParser implements ModuleParser {
                         ModuleDefinition moduleDefinition = new ModuleDefinition(resource, manifest);
                         nameModuleDefinitionMap.put(moduleName, moduleDefinition);
                         uriModuleDefinitionMap.put(uriForMatch, moduleDefinition);
+                        // for test
                         String uri = uriForMatch.toString();
                         if (uri.endsWith("/target/classes/")) {
                             uri = StrUtil.removeSuffix(uri, "/target/classes/");
