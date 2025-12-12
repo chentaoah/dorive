8676dfe0bcc1a3ad90ee06d8d59cb9195446ff3b
chentaoah <609580885@qq.com>
2025-12-12 22:36:24 +0800

    简化代码

--- dorive-launcher/src/main/java/com/gitee/dorive/launcher/v1/Test.java
+++ dorive-launcher/src/main/java/com/gitee/dorive/launcher/v1/Test.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.gitee.dorive.launcher.v1;
 
 import java.nio.file.Files;
@@ -12,11 +29,11 @@ import java.util.stream.Stream;
 public class Test {
 
     public static void main(String[] args) {
-        String inputPath1 = "C:\\Users\\Administrator\\Desktop\\Cloud\\dorive";
+        String inputPath1 = "D:\\Work\\CloudSpace\\dorive";
         Set<String> filenames1 = new LinkedHashSet<>();
         resolve(inputPath1, filenames1);
 
-        String inputPath2 = "C:\\Users\\Administrator\\Desktop\\Cloud\\test\\dorive";
+        String inputPath2 = "C:\\Users\\chenT\\Desktop\\new\\dorive";
         Set<String> filenames2 = new LinkedHashSet<>();
         resolve(inputPath2, filenames2);
 
