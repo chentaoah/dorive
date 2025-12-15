2b030d40d0fb2349234c0ec6f7d09a45469925a9
chentaoah <609580885@qq.com>
2025-12-15 20:48:06 +0800

    优化判断逻辑

--- dorive-joiner/src/main/java/com/gitee/dorive/joiner/v1/impl/joiner/DefaultEntityJoiner.java
+++ dorive-joiner/src/main/java/com/gitee/dorive/joiner/v1/impl/joiner/DefaultEntityJoiner.java
@@ -1,5 +1,23 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.gitee.dorive.joiner.v1.impl.joiner;
 
+import com.gitee.dorive.base.v1.binder.api.Binder;
 import com.gitee.dorive.base.v1.binder.api.BinderExecutor;
 import com.gitee.dorive.base.v1.common.entity.EntityElement;
 import com.gitee.dorive.base.v1.common.enums.JoinType;
@@ -29,13 +47,14 @@ public class DefaultEntityJoiner implements EntityJoiner {
         EntityElement entityElement = repository.getEntityElement();
         BinderExecutor binderExecutor = repository.getBinderExecutor();
         JoinType joinType = binderExecutor.getJoinType();
+        List<Binder> binders = binderExecutor.getRootStrongBinders();
         if (joinType == JoinType.SINGLE) {
-            this.keyGen1 = new SingleEntityKeyGenerator(binderExecutor.getRootStrongBinders().get(0));
-            this.keyGen2 = new SingleFieldKeyGenerator(binderExecutor.getRootStrongBinders().get(0));
+            this.keyGen1 = new SingleEntityKeyGenerator(binders.get(0));
+            this.keyGen2 = new SingleFieldKeyGenerator(binders.get(0));
 
         } else if (joinType == JoinType.MULTI) {
-            this.keyGen1 = new MultiEntityKeyGenerator(binderExecutor.getRootStrongBinders());
-            this.keyGen2 = new MultiFieldKeyGenerator(binderExecutor.getRootStrongBinders());
+            this.keyGen1 = new MultiEntityKeyGenerator(binders);
+            this.keyGen2 = new MultiFieldKeyGenerator(binders);
         }
         this.setter = (entity, object) -> {
             Object value = entityElement.getValue(entity);
