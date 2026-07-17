eb848b35418d493d56f20aeca8f7cfd056d1a934
chentaoah <609580885@qq.com>
2026-07-17 12:30:53 +0800

    微调

--- dorive-web/src/main/java/com/gitee/dorive/web/v1/impl/service/Validator.java
+++ dorive-web/src/main/java/com/gitee/dorive/web/v1/impl/service/Validator.java
@@ -1,9 +1,25 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.gitee.dorive.web.v1.impl.service;
 
 import cn.hutool.core.annotation.AnnotationUtil;
 import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.util.ReflectUtil;
-import com.gitee.dorive.base.v1.common.entity.EntityElement;
 import com.gitee.dorive.base.v1.core.api.Options;
 import com.gitee.dorive.base.v1.core.entity.qry.Example;
 import com.gitee.dorive.base.v1.web.annotation.UniqueConstraint;
@@ -31,7 +47,7 @@ public class Validator {
 
     public <E> Validator(AbstractQueryRepository<E, Object> repository, Class<?> entityClass, Function<Result, Result> function) {
         this.repository = repository;
-        this.uniqueConstraintFields = new ArrayList<>();
+        this.uniqueConstraintFields = new ArrayList<>(4);
         for (Field field : ReflectUtil.getFields(entityClass)) {
             if (AnnotationUtil.hasAnnotation(field, UniqueConstraint.class)) {
                 uniqueConstraintFields.add(field);
